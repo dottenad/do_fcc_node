e08e4aa1120c5038221dfce3b5d83ec22e725dc2
dottenad <ottenadd@gmail.com>
2022-03-03 11:08:45 -0800

    all

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
@@ -8,9 +9,15 @@ app.get("/", function (req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({
+      message: "HELLO JSON",
+    });
+  } else {
+    res.json({
+      message: "Hello json",
+    });
+  }
 });
 
 module.exports = app;
